576e0aab9a9c250441e54e7553eb1c37bd346f55
TiagoMateragia <tiagomateragia27@gmail.com>
2026-03-13 20:18:23 -0300

    13/03 Localização do usuário
    Função para pegar a latitude e longitude do usuário programada com sucesso.

---
+++ javascript/climatizador.js
@@ -0,0 +1,48 @@
+// Variáveis para ligação com o HTML.
+let nome_dia_um = document.querySelector('.nome_dia_um');
+let graus_dia_um = document.querySelector('.graus_dia_um');
+let imagem_dia_um = document.querySelector('.imagem_dia_um');
+let nome_dia_dois = document.querySelector('.nome_dia_dois');
+let graus_dia_dois = document.querySelector('.graus_dia_dois');
+let imagem_dia_dois = document.querySelector('.imagem_dia_dois');
+let nome_dia_tres = document.querySelector('.nome_dia_tres');
+let graus_dia_tres = document.querySelector('.graus_dia_tres');
+let imagem_dia_tres = document.querySelector('.imagem_dia_tres');
+let nome_dia_quatro = document.querySelector('.nome_dia_quatro');
+let graus_dia_quatro = document.querySelector('.graus_dia_quatro');
+let imagem_dia_quatro = document.querySelector('.imagem_dia_quatro');
+let nome_dia_cinco = document.querySelector('.nome_dia_cinco');
+let graus_dia_cinco = document.querySelector('.graus_dia_cinco');
+let imagem_dia_cinco = document.querySelector('.imagem_dia_cinco');
+//Função para pegar a latitude e longitude do usuário.
+function pegar_local(){
+//Define um obj com a latitude e longitude encontrada na posição do usuário.
+    let achar_posicao = posicao =>{
+        let dados = {
+            latitude: posicao.coords.latitude,
+            longitude: posicao.coords.longitude
+        }
+    }
+//Mensagem de erro caso o usuário negue a permissão.
+    let local_erro = erro =>{
+        nome_dia_um.innerHTML = "Acesso não concedido.";
+        graus_dia_um.innerHTML = "Acesso não concedido.";
+        imagem_dia_um.innerHTML = "Acesso não concedido.";
+        nome_dia_dois.innerHTML = "Acesso não concedido.";
+        graus_dia_dois.innerHTML = "Acesso não concedido.";
+        imagem_dia_dois.innerHTML = "Acesso não concedido.";
+        nome_dia_tres.innerHTML = "Acesso não concedido.";
+        graus_dia_tres.innerHTML = "Acesso não concedido.";
+        imagem_dia_tres.innerHTML = "Acesso não concedido.";
+        nome_dia_quatro.innerHTML = "Acesso não concedido.";
+        graus_dia_quatro.innerHTML = "Acesso não concedido.";
+        imagem_dia_quatro.innerHTML = "Acesso não concedido.";
+        nome_dia_cinco.innerHTML = "Acesso não concedido.";
+        graus_dia_cinco.innerHTML = "Acesso não concedido.";
+        imagem_dia_cinco.innerHTML = "Acesso não concedido.";
+    }
+//Solicita permissão e encontra a posição do usuário.
+    navigator.geolocation.getCurrentPosition(achar_posicao, local_erro);
+}
+
+pegar_local()
