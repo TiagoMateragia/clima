dbc4b25b4407aa3bc81df1c5b3381f3993f99451
TiagoMateragia <tiagomateragia27@gmail.com>
2026-03-16 20:32:40 -0300

    16/03 Integrando a API
    API Fetch programada e retornando o JSON corretamente, faltando programar a visualização dos dados para o usuário.

--- javascript/climatizador.js
+++ javascript/climatizador.js
@@ -14,6 +14,7 @@ let imagem_dia_quatro = document.querySelector('.imagem_dia_quatro');
 let nome_dia_cinco = document.querySelector('.nome_dia_cinco');
 let graus_dia_cinco = document.querySelector('.graus_dia_cinco');
 let imagem_dia_cinco = document.querySelector('.imagem_dia_cinco');
+
 //Função para pegar a latitude e longitude do usuário.
 function pegar_local(){
 //Define um obj com a latitude e longitude encontrada na posição do usuário.
@@ -22,6 +23,7 @@ function pegar_local(){
             latitude: posicao.coords.latitude,
             longitude: posicao.coords.longitude
         }
+        mostrar_clima(dados);
     }
 //Mensagem de erro caso o usuário negue a permissão.
     let local_erro = erro =>{
@@ -45,4 +47,15 @@ function pegar_local(){
     navigator.geolocation.getCurrentPosition(achar_posicao, local_erro);
 }
 
-pegar_local()
+//Função para mostrar as informações do clima.
+function mostrar_clima(dados){
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,weathercode&timezone=auto")
+    .then((dado) =>{
+        return dado.json();
+    })
+    .then((dado_json) =>{
+        console.log(dado_json);
+    })
+}
+
+pegar_local();
